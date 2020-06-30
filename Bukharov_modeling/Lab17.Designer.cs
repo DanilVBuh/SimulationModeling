f545c559916fbac51a9689158a82733a25455c49
Данил Бухаров <danilvbuh@gmail.com>
2020-07-01 00:19:22 +0700

    done lab 17

--- Bukharov_modeling/Lab17.Designer.cs
+++ Bukharov_modeling/Lab17.Designer.cs
@@ -37,6 +37,17 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.startBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timeValue = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.varianceError = new System.Windows.Forms.Label();
+            this.averageError = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chi = new System.Windows.Forms.Label();
+            this.averageValue = new System.Windows.Forms.Label();
+            this.varianceValue = new System.Windows.Forms.Label();
+            this.statBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -90,7 +101,7 @@
             // 
             // startBtn
             // 
-            this.startBtn.Location = new System.Drawing.Point(12, 421);
+            this.startBtn.Location = new System.Drawing.Point(12, 415);
             this.startBtn.Name = "startBtn";
             this.startBtn.Size = new System.Drawing.Size(118, 23);
             this.startBtn.TabIndex = 16;
@@ -98,11 +109,118 @@
             this.startBtn.UseVisualStyleBackColor = true;
             this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
             // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(595, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Time: ";
+            // 
+            // timeValue
+            // 
+            this.timeValue.AutoSize = true;
+            this.timeValue.Location = new System.Drawing.Point(634, 329);
+            this.timeValue.Name = "timeValue";
+            this.timeValue.Size = new System.Drawing.Size(14, 13);
+            this.timeValue.TabIndex = 33;
+            this.timeValue.Text = "T";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(595, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Chi-squared: ";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(595, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Average: ";
+            // 
+            // varianceError
+            // 
+            this.varianceError.AutoSize = true;
+            this.varianceError.Location = new System.Drawing.Point(719, 286);
+            this.varianceError.Name = "varianceError";
+            this.varianceError.Size = new System.Drawing.Size(26, 13);
+            this.varianceError.TabIndex = 27;
+            this.varianceError.Text = "Verr";
+            // 
+            // averageError
+            // 
+            this.averageError.AutoSize = true;
+            this.averageError.Location = new System.Drawing.Point(719, 269);
+            this.averageError.Name = "averageError";
+            this.averageError.Size = new System.Drawing.Size(26, 13);
+            this.averageError.TabIndex = 26;
+            this.averageError.Text = "Aerr";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(595, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Variance: ";
+            // 
+            // chi
+            // 
+            this.chi.AutoSize = true;
+            this.chi.Location = new System.Drawing.Point(668, 312);
+            this.chi.Name = "chi";
+            this.chi.Size = new System.Drawing.Size(14, 13);
+            this.chi.TabIndex = 31;
+            this.chi.Text = "X";
+            // 
+            // averageValue
+            // 
+            this.averageValue.AutoSize = true;
+            this.averageValue.Location = new System.Drawing.Point(654, 269);
+            this.averageValue.Name = "averageValue";
+            this.averageValue.Size = new System.Drawing.Size(28, 13);
+            this.averageValue.TabIndex = 28;
+            this.averageValue.Text = "Aval";
+            // 
+            // varianceValue
+            // 
+            this.varianceValue.AutoSize = true;
+            this.varianceValue.Location = new System.Drawing.Point(654, 286);
+            this.varianceValue.Name = "varianceValue";
+            this.varianceValue.Size = new System.Drawing.Size(28, 13);
+            this.varianceValue.TabIndex = 29;
+            this.varianceValue.Text = "Vval";
+            // 
+            // statBtn
+            // 
+            this.statBtn.Location = new System.Drawing.Point(670, 415);
+            this.statBtn.Name = "statBtn";
+            this.statBtn.Size = new System.Drawing.Size(118, 23);
+            this.statBtn.TabIndex = 34;
+            this.statBtn.Text = "Stat";
+            this.statBtn.UseVisualStyleBackColor = true;
+            this.statBtn.Click += new System.EventHandler(this.statBtn_Click);
+            // 
             // Lab17
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.timeValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.varianceError);
+            this.Controls.Add(this.averageError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chi);
+            this.Controls.Add(this.averageValue);
+            this.Controls.Add(this.varianceValue);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.startBtn);
             this.Name = "Lab17";
@@ -110,6 +228,7 @@
             this.Load += new System.EventHandler(this.Lab17_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -118,5 +237,16 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timeValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label varianceError;
+        private System.Windows.Forms.Label averageError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label chi;
+        private System.Windows.Forms.Label averageValue;
+        private System.Windows.Forms.Label varianceValue;
+        private System.Windows.Forms.Button statBtn;
     }
 }
--- Bukharov_modeling/Lab17.cs
+++ Bukharov_modeling/Lab17.cs
@@ -14,19 +14,34 @@ namespace Bukharov_modeling
     {
         Random random = new Random();
 
+        public int N = 1000;
+        public double Average;
+        public double AverageReal;
+        public double DAv;
+        public double Variance;
+        public double VarianceReal;
+        public double DVa;
+        public double X2;
+        public double X2Real = 19.7;
+        public double a = 0.5;
+        public double d = 1, min, max, l;
+        public int n = 12, k;
+        public int Index;
+        public int m = 0;
+
+        public ICollection<double> Results = new List<double>();
+        public List<int> preRes = new List<int>();
+        public List<int> Answers;
+
         double t01 = 0;
         double t1 = 0;
         double t02 = 0;
         double t2 = 0;
-        double t0 = 0;
-        double t = 0;
         double T = 50;
         double lambda1 = 0.5;
         double lambda2 = 1.5;
-        double lambda;
         public Lab17()
         {
-            lambda = lambda1 + lambda2;
             InitializeComponent();
             timer1.Interval = 100;
         }
@@ -39,10 +54,9 @@ namespace Bukharov_modeling
             this.t01 = 0;
             this.t2 = 0;
             this.t02 = 0;
-            this.t = 0;
-            this.t0 = 0;
             timer1.Start();
             this.Calculate();
+
         }
         public void Calculate()
         {
@@ -56,6 +70,7 @@ namespace Bukharov_modeling
                 {
                     this.chart1.Series[0].Points.AddXY(t1, 3);
                     this.chart1.Series[2].Points.AddXY(t1, 1);
+                    //preRes.Add(t1);
                     t01 = t1;
                 }
             }
@@ -69,8 +84,110 @@ namespace Bukharov_modeling
                 {
                     this.chart1.Series[1].Points.AddXY(t2, 2);
                     this.chart1.Series[2].Points.AddXY(t2, 1);
+                    //preRes.Add(t2);
                     t02 = t2;
                 }
+            }
+        }
+
+        public void Stat()
+        {
+            while (t1 <= T)
+            {
+                double a = random.NextDouble();
+
+                a = random.NextDouble();
+                t1 = t01 - Math.Log(a) / lambda1;
+                if (t1 <= T)
+                {
+                    m++;
+                    //preRes.Add(t1);
+                    t01 = t1;
+                }
+            }
+            while (t2 <= T)
+            {
+                double a = random.NextDouble();
+
+                a = random.NextDouble();
+                t2 = t02 - Math.Log(a) / lambda1;
+                if (t2 <= T)
+                {
+                    m++;
+                    //preRes.Add(t2);
+                    t02 = t2;
+                }
+            }
+        }
+
+        private void statBtn_Click(object sender, EventArgs e)
+        {
+            this.Average = 0;
+            this.Variance = 0;
+            this.AverageReal = 0;
+            this.VarianceReal = 0;
+            this.X2 = 0;
+            this.Results.Clear();
+            this.t1 = 0;
+            this.t01 = 0;
+            this.t2 = 0;
+            this.t02 = 0;
+            this.fun();
+        }
+
+        public void fun()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                this.Stat();
+                double C = m;
+                Results.Add(C);
+                Average += C;
+                Variance += C * C;
+                if (i == 0)
+                    min = max = C;
+                if (C < min)
+                    min = C;
+                if (C > max)
+                    max = C;
+                m = 0;
+            }
+            k = (int)Math.Ceiling(Math.Sqrt(N) / 4) + 1;
+            l = (max - min) / k;
+            Average /= N;
+            Variance = Variance / N - Average * Average;
+            Answers = new List<int>(new int[k + 1]);
+            foreach (double c in Results)
+            {
+                double interval = (c - min) / l;
+                Answers[(int)interval]++;
+            }
+            for (int j = 0; j < k; j++)
+            {
+                double f = (double)Answers[j] / N;
+            }
+            for (int j = 0; j < k; j++)
+            {
+                double mid = j * l + min + l / 2;
+                double p = l * Math.Exp(-Math.Pow(mid - a, 2) / (2 * d)) / Math.Sqrt(d * 2 * Math.PI);
+
+                X2 += Answers[j] * Answers[j] / (N * p);
+            }
+            X2 -= N;
+            averageValue.Text = Average.ToString("F6");
+            varianceValue.Text = Variance.ToString("F6");
+            DVa = Math.Abs(d - Variance) / d * 100;
+            DAv = Math.Abs(a - Average) / a * 100;
+            averageError.Text = "(error = " + DAv.ToString("F4") + "%)";
+            varianceError.Text = "(error = " + DVa.ToString("F4") + "%)";
+
+            if (X2 > X2Real)
+            {
+                chi.Text = X2.ToString("F3") + " > " + X2Real.ToString("F3") + " is false";
+            }
+            else
+            {
+                chi.Text = X2.ToString("F3") + " <= " + X2Real.ToString("F3") + " is true";
             }
         }
         private void startBtn_Click(object sender, EventArgs e)
