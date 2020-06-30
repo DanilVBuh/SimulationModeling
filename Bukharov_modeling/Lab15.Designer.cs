a8c243d93ddb81b9403e633f7c10d94e99cdaf5a
Данил Бухаров <danilvbuh@gmail.com>
2020-06-30 23:44:37 +0700

    done lab 15

--- Bukharov_modeling/Lab15.Designer.cs
+++ Bukharov_modeling/Lab15.Designer.cs
@@ -29,12 +29,20 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.startBtn = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label33 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,28 +58,28 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Cursor = System.Windows.Forms.Cursors.Default;
             this.chart1.Dock = System.Windows.Forms.DockStyle.Top;
-            legend1.Enabled = false;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
-            series1.BorderWidth = 3;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
-            series1.Color = System.Drawing.Color.Green;
-            series1.IsXValueIndexed = true;
-            series1.Legend = "Legend1";
-            series1.MarkerColor = System.Drawing.Color.Blue;
-            series1.Name = "Series1";
-            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            series1.YValuesPerPoint = 6;
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(800, 258);
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series2.Color = System.Drawing.Color.Green;
+            series2.IsXValueIndexed = true;
+            series2.Legend = "Legend1";
+            series2.MarkerColor = System.Drawing.Color.Blue;
+            series2.Name = "Series1";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValuesPerPoint = 6;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(800, 206);
             this.chart1.TabIndex = 15;
             this.chart1.Text = "chart1";
             // 
@@ -79,11 +87,92 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // statBtn
+            // 
+            this.statBtn.Location = new System.Drawing.Point(670, 415);
+            this.statBtn.Name = "statBtn";
+            this.statBtn.Size = new System.Drawing.Size(118, 23);
+            this.statBtn.TabIndex = 16;
+            this.statBtn.Text = "Stat";
+            this.statBtn.UseVisualStyleBackColor = true;
+            this.statBtn.Click += new System.EventHandler(this.statBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(652, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "пасмурно(3)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(652, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "облачно(2)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(652, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "ясно(1)";
+            // 
+            // label33
+            // 
+            this.label33.AutoSize = true;
+            this.label33.Location = new System.Drawing.Point(726, 375);
+            this.label33.Name = "label33";
+            this.label33.Size = new System.Drawing.Size(0, 10);
+            this.label33.TabIndex = 22;
+            this.label33.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(726, 349);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(0, 10);
+            this.label22.TabIndex = 21;
+            this.label22.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(726, 323);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 10);
+            this.label11.TabIndex = 20;
+            this.label11.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(667, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Частоты:";
+            // 
             // Lab15
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label33);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statBtn);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.startBtn);
             this.Name = "Lab15";
@@ -91,6 +180,7 @@
             this.Load += new System.EventHandler(this.Lab15_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,5 +189,13 @@
         private System.Windows.Forms.Button startBtn;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button statBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label33;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Bukharov_modeling/Lab15.cs
+++ Bukharov_modeling/Lab15.cs
@@ -17,6 +17,8 @@ namespace Bukharov_modeling
         public double nextT = 0;
         public double dt = 1;
         public int i0 = 0;
+        public int N = 100;
+        public List<double> res;
         public List<List<double>> q = new List<List<double>>
         {
             new List<double>{-0.4, 0.3, 0.1},
@@ -24,24 +26,11 @@ namespace Bukharov_modeling
             new List<double>{0.1, 0.4, -0.5},
         };
 
-        ICollection<Event> events = new List<Event>();
-        public class Event
-        {
-
-            public double t { get; set; }
-            public int m { get; set; }
-
-            public Event(double t, int m)
-            {
-                this.t = t;
-                this.m = m;
-            }
-
-        }
         public Lab15()
         {
             InitializeComponent();
             timer1.Interval = 100;
+            res = new List<double>(new double[3]);
         }
         public void Reload()
         {
@@ -49,7 +38,6 @@ namespace Bukharov_modeling
             this.T = 0;
             this.nextT = 0;
             this.i0 = 0;
-            this.events.Clear();
             timer1.Start();
         }
 
@@ -88,6 +76,45 @@ namespace Bukharov_modeling
             }
         }
 
+        public void Stat()
+        {
+            for (int k = 0; k < N; k++)
+            {
+                while (T <= 24)
+                {
+                    double a = random.NextDouble();
+                    double t = Math.Log(a) / q[i0][i0];
+                    T += t;
+                    a = random.NextDouble();
+                    List<double> p = new List<double>();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (i0 != j)
+                        {
+                            p.Add(-1 * q[i0][j] / q[i0][i0]);
+                        }
+                        else
+                        {
+                            p.Add(0);
+                        }
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        a -= p[i];
+                        if (a < 0)
+                        {
+                            i0 = i;
+                            break;
+                        }
+                    }
+                }
+                res[i0]++;
+                this.i0 = 0;
+                this.T = 0;
+                this.nextT = 0;
+            }
+        }
+
         private void startBtn_Click(object sender, EventArgs e)
         {
             this.Reload();
@@ -103,6 +130,37 @@ namespace Bukharov_modeling
             if (T > 24)
                 timer1.Stop();
             this.Calculate();
+        }
+
+        private void statBtn_Click(object sender, EventArgs e)
+        {
+            this.Stat();
+            for (int i = 0; i < 3; i++)
+            {
+                res[i] /= N;
+            }
+            label11.Text = res[0].ToString("F3");
+            label22.Text = res[1].ToString("F3");
+            label33.Text = res[2].ToString("F3");
+            for (int i = 0; i < 3; i++)
+            {
+                res[i] = 0;
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
